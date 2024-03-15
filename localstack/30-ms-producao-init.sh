644599fe50481172e9aed1ee9ce65572a0a29202
Leandro Gonçalves de Oliveira <olegon.main+github@gmail.com>
2024-03-15 13:42:42 -0300

    fix: fixes server port

--- localstack/30-ms-producao-init.sh
+++ localstack/30-ms-producao-init.sh
@@ -14,3 +14,29 @@ awslocal sns subscribe \
   --protocol sqs \
   --notification-endpoint arn:aws:sqs:us-east-1:000000000000:ms-producao-evento-pedido-recebido \
   --attributes '{ "RawMessageDelivery": "true" }'
+
+awslocal sqs create-queue \
+  --queue-name ms-producao-evento-pagamento-aprovado-dlq
+
+awslocal sqs create-queue \
+  --queue-name ms-producao-evento-pagamento-aprovado \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\": \"arn:aws:sqs:us-east-1:000000000000:ms-producao-evento-pagamento-aprovado-dlq\", \"maxReceiveCount\": \"3\" }" }'
+
+awslocal sns subscribe \
+  --topic-arn arn:aws:sns:us-east-1:000000000000:ms-pagamento \
+  --protocol sqs \
+  --notification-endpoint arn:aws:sqs:us-east-1:000000000000:ms-producao-evento-pagamento-aprovado \
+  --attributes '{ "RawMessageDelivery": "true", "FilterPolicy": "{\"event-type\": [\"aprovado\"]}" }'
+
+awslocal sqs create-queue \
+  --queue-name ms-producao-evento-pagamento-recusado-dlq
+
+awslocal sqs create-queue \
+  --queue-name ms-producao-evento-pagamento-recusado \
+  --attributes '{ "RedrivePolicy": "{\"deadLetterTargetArn\": \"arn:aws:sqs:us-east-1:000000000000:ms-producao-evento-pagamento-recusado-dlq\", \"maxReceiveCount\": \"3\" }" }'
+
+awslocal sns subscribe \
+  --topic-arn arn:aws:sns:us-east-1:000000000000:ms-pagamento \
+  --protocol sqs \
+  --notification-endpoint arn:aws:sqs:us-east-1:000000000000:ms-producao-evento-pagamento-recusado \
+  --attributes '{ "RawMessageDelivery": "true", "FilterPolicy": "{\"event-type\": [\"recusado\"]}" }'
